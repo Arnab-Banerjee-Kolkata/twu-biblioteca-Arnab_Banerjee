628dbc8fb0d3e67a8af512c81143f964c448502b
Arnab Banerjee <arnabb547@gmail.com>
2020-02-07 18:38:17 +0530

    Add. Show welcome message when app is started.

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -1,3 +1,11 @@
 public class BibliotecaApp {
 
+
+    public BibliotecaApp() {
+        this.showWelcomeMessage();
+    }
+
+    public String showWelcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
 }
--- src/test/java/BibliotecaAppTest.java
+++ src/test/java/BibliotecaAppTest.java
@@ -1,4 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class BibliotecaAppTest {
+
+    @Test
+    void shouldShowWelcomeMessageWhenAppIsStarted() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        String welcomeMessage = bibliotecaApp.showWelcomeMessage();
+
+        Assertions.assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
+    }
 
 
 }
