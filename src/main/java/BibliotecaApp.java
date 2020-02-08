1746a5614dca85bb9ed8c109c0ea0dfbce7dc849
Arnab Banerjee <arnabb547@gmail.com>
2020-02-08 10:17:24 +0530

    Requirement-1.2: |Add. Show a list of available books after displaying the welcome message|

--- src/main/java/BibliotecaApp.java
+++ src/main/java/BibliotecaApp.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BibliotecaApp {
 
+    private List<String> books;
 
     public BibliotecaApp() {
         this.showWelcomeMessage();
+        this.books = new ArrayList<>(Arrays.asList(
+                "Book_Name_1",
+                "Book_Name_2"
+        ));
+        this.showBooks();
     }
 
     public String showWelcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public List<String> showBooks() {
+        return this.books;
     }
 }
--- src/test/java/BibliotecaAppTest.java
+++ src/test/java/BibliotecaAppTest.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BibliotecaAppTest {
 
     @Test
@@ -9,8 +15,24 @@ class BibliotecaAppTest {
 
         String welcomeMessage = bibliotecaApp.showWelcomeMessage();
 
-        Assertions.assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
+        assertEquals("Welcome to Biblioteca." +
+                " Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
     }
 
+    @Test
+    void shouldShowAListOfAvailableBooksAfterDisplayingWelcomeMessage() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        List<String> expectedBooks = new ArrayList<>(Arrays.asList(
+                "Book_Name_1",
+                "Book_Name_2"
+        ));
 
+        String welcomeMessage = bibliotecaApp.showWelcomeMessage();
+        List<String> actualBooks = bibliotecaApp.showBooks();
+
+        assertEquals("Welcome to Biblioteca." +
+                " Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
+        assertEquals(expectedBooks, actualBooks);
+
+    }
 }
