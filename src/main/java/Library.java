24a136bf7ef68a033ae4f7e3633943e2c3d29347
Arnab Banerjee <arnabb547@gmail.com>
2020-02-11 08:04:20 +0530

    |Add. User can check out books from Library|

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,11 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Library {
     private List<Book> books;
+    private List<Book> checkOutBooks;
+    private PrintStream outputStream;
 
-    Library() {
+    Library(PrintStream outputStream) {
+        this.outputStream = outputStream;
+        checkOutBooks = new ArrayList<>();
         this.books = new ArrayList<>(Arrays.asList(
                 new Book("Book_Name_1", "Author_Name_1", 1998),
                 new Book("Book_Name_2", "Author_Name_2", 1987)
@@ -13,10 +21,33 @@ public class Library {
     }
 
     public List<String> showBooks() {
-        List<String> bookTitles = new ArrayList<>();
+        List<String> bookDetails = new ArrayList<>();
         for (Book book : books) {
-            bookTitles.add(book.getDetails());
+            bookDetails.add(book.getDetails());
         }
-        return bookTitles;
+        return bookDetails;
+    }
+
+    public String checkOutBook() throws IOException {
+        String message;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        outputStream.println("Enter title");
+        String bookTitle = bufferedReader.readLine();
+        outputStream.println("Enter author name");
+        String authorName = bufferedReader.readLine();
+        outputStream.println("Enter year of publishing");
+        try {
+            int yearOfPublishing = Integer.parseInt(bufferedReader.readLine());
+            Book book = new Book(bookTitle, authorName, yearOfPublishing);
+            if (books.contains(book)) {
+                message = "Thank you! Enjoy the book";
+                checkOutBooks.add(book);
+            } else
+                message = "Sorry, that book is not available";
+            books.remove(book);
+        } catch (NumberFormatException e) {
+            message = "Invalid Input";
+        }
+        return message;
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,8 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,7 +15,7 @@ class LibraryTest {
 
     @BeforeEach
     void setup() {
-        library = new Library();
+        library = new Library(System.out);
     }
 
     @Test
@@ -25,5 +28,18 @@ class LibraryTest {
 
         assertEquals(expectedBooks, actualBooks);
 
+    }
+
+    @Test
+    void shouldLetUserCheckoutABook() throws IOException {
+        Book book1 = new Book("Book_Name_1", "Author_Name_1", 1998);
+        List<String> expectedBooks = new ArrayList<>(Collections.singletonList(book1.getDetails()));
+        String simulatedInput = "Book_Name_2\nAuthor_Name_2\n1987";
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+        library.checkOutBook();
+        List<String> actualBooks = library.showBooks();
+
+        assertEquals(expectedBooks, actualBooks);
     }
 }
