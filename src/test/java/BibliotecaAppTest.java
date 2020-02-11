1f3b20ac320f31505b593c62bf5e99b297a34964
Arnab Banerjee <arnabb547@gmail.com>
2020-02-11 17:22:45 +0530

    |Refactor. Removing test of responsibility of library from BibliotecaAppTest|

--- src/test/java/BibliotecaAppTest.java
+++ src/test/java/BibliotecaAppTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BibliotecaAppTest {
@@ -23,14 +19,5 @@ class BibliotecaAppTest {
                 " Your one-stop-shop for great book titles in Bangalore!", welcomeMessage);
     }
 
-    @Test
-    void shouldShowListOfBooksWithTitleAuthorAndPublishedYear() {
-        Book book1 = new Book("Book_Name_1", "Author_Name_1", 1998);
-        Book book2 = new Book("Book_Name_2", "Author_Name_2", 1987);
-        List<String> expectedBooks = new ArrayList<>(Arrays.asList(book1.getDetails(), book2.getDetails()));
 
-        List<String> actualBooks = bibliotecaApp.showBooks();
-
-        assertEquals(expectedBooks, actualBooks);
-    }
 }
