8f75c66549bbba9ee5a41038907eb033f90a79d8
Arnab Banerjee <arnabb547@gmail.com>
2020-02-12 10:42:23 +0530

    |Add. Console can print to consumer|

--- src/test/java/ConsoleTest.java
+++ src/test/java/ConsoleTest.java
@@ -1,14 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class ConsoleTest {
+    Console console;
+
+    @BeforeEach
+    void setup() {
+        console = new Console(System.out);
+    }
+
     @Test
     void shouldReadInputFromConsumer() throws IOException {
-        Console console = new Console();
         String simulatedInput = "Name: ThoughtWorks";
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
         String expectedValue = "Name: ThoughtWorks";
@@ -16,5 +25,15 @@ class ConsoleTest {
         String actualValue = console.readLine();
 
         assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    void shouldPrintToConsumer() {
+        PrintStream printStream = mock(PrintStream.class);
+        console = new Console(printStream);
+
+        console.print("Name: ThoughtWorks");
+
+        verify(printStream, times(1)).print("Name: ThoughtWorks");
     }
 }
